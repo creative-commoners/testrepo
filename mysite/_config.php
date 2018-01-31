82ab717178d116dafba15d3f4063fca81363092d
Dylan Wagstaff <dylan@silverstripe.com>
2018-01-31 16:28:30 +1300

    Upgrade for upcoming CWP 2.0 release
    Also expand test suite to encompass all default modules
    (at least covering cwp-recipe-core). Remove all now unnecessary support
    files, such as .htaccess which is now provided by a dependency.

--- mysite/_config.php
+++ mysite/_config.php
@@ -1,25 +1,9 @@
 <?php
-
-global $project;
-$project = 'mysite';
-
-global $database;
+use SilverStripe\Core\Environment;
 
 // find the database name from the environment file
-if(defined('SS_DATABASE_NAME') && SS_DATABASE_NAME) {
-	$database = SS_DATABASE_NAME;
-} else {
-	$database = 'SS_cwp';
+if(empty(Environment::getEnv('SS_DATABASE_NAME'))) {
+	Environment::setEnv('SS_DATABASE_NAME', 'SS_cwp');
 }
 
-require_once('conf/ConfigureFromEnv.php');
-
 date_default_timezone_set('Pacific/Auckland');
-
-// Set default FROM field for emails. Reconfigure this if you would like to use a custom address.
-Config::inst()->update('Email', 'admin_email', 'no-reply@cwp.govt.nz');
-
-## NOTE: Any SilverStripe configuration ideally goes into mysite/_config/config.yml
-## which uses the {@link Config} API instead of manipulating statics directly.
-## Check out "configuration.md" in the framework docs for more information.
-
