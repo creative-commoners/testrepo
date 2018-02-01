33b2128aa60716bb917ccb761d718ce71d9fe37c
Dylan Wagstaff <dylan@silverstripe.com>
2018-02-01 13:53:33 +1300

    Reinstate Pages, alter configuration layout
    Make test groups more granular, add phpcs config, and other tidy ups.

--- mysite/_config.php
+++ mysite/_config.php
@@ -2,8 +2,8 @@
 use SilverStripe\Core\Environment;
 
 // find the database name from the environment file
-if(empty(Environment::getEnv('SS_DATABASE_NAME'))) {
-	Environment::setEnv('SS_DATABASE_NAME', 'SS_cwp');
+if (empty(Environment::getEnv('SS_DATABASE_NAME'))) {
+    Environment::setEnv('SS_DATABASE_NAME', 'SS_cwp');
 }
 
 date_default_timezone_set('Pacific/Auckland');
