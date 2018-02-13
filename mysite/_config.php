00cc39e9fa2314a3bef26d8e543dc8ee719c7c1f
Dylan <dylan@silverstripe.com>
2018-02-13 21:44:06 +1300

    Remove obsolete _config.php file
    It was only filled with a fallback that no longer works in SilverStripe
    Four, and a development type shim that should also be taken care of in
    the runtime/deployment environment (be it development or production).

--- mysite/_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-use SilverStripe\Core\Environment;
-
-// find the database name from the environment file
-if (empty(Environment::getEnv('SS_DATABASE_NAME'))) {
-    Environment::setEnv('SS_DATABASE_NAME', 'SS_cwp');
-}
-
-date_default_timezone_set('Pacific/Auckland');
