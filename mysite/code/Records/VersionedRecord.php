83fb2add3469e14d1ea5c3ef421c32805e8a3776
Robbie Averill <robbie@silverstripe.com>
2018-03-20 15:40:59 +1300

    Add specific permission checks for VersionedRecord against access to its ModelAdmin

--- mysite/code/Records/VersionedRecord.php
+++ mysite/code/Records/VersionedRecord.php
@@ -4,6 +4,7 @@ namespace Mysite\Project\Records;
 
 use Page;
 use SilverStripe\ORM\DataObject;
+use SilverStripe\Security\Permission;
 use SilverStripe\Versioned\Versioned;
 
 class VersionedRecord extends DataObject
@@ -26,4 +27,36 @@ class VersionedRecord extends DataObject
         Versioned::DRAFT,
         Versioned::LIVE,
     ];
+
+    public function canView($member = null)
+    {
+        if (Permission::check('CMS_ACCESS_Mysite\\Project\\Admin\\VersionedRecordAdmin')) {
+            return true;
+        }
+        return parent::canView($member);
+    }
+
+    public function canEdit($member = null)
+    {
+        if (Permission::check('CMS_ACCESS_Mysite\\Project\\Admin\\VersionedRecordAdmin')) {
+            return true;
+        }
+        return parent::canEdit($member);
+    }
+
+    public function canCreate($member = null, $context = [])
+    {
+        if (Permission::check('CMS_ACCESS_Mysite\\Project\\Admin\\VersionedRecordAdmin')) {
+            return true;
+        }
+        return parent::canCreate($member, $context);
+    }
+
+    public function canDelete($member = null)
+    {
+        if (Permission::check('CMS_ACCESS_Mysite\\Project\\Admin\\VersionedRecordAdmin')) {
+            return true;
+        }
+        return parent::canDelete($member);
+    }
 }
