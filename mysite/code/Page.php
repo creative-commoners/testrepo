0a3d073958bebb72a6182fa85aac240314cd40fb
Robbie Averill <robbie@silverstripe.com>
2018-04-16 12:10:34 +1200

    Add taxonomy directory capability

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -4,5 +4,7 @@ use CWP\CWP\PageTypes\BasePage;
 
 class Page extends BasePage
 {
-    
+    private static $db = [
+        'Foo' => 'Varchar', // Required for example only to ensure there's a DB table for Page
+    ];
 }
