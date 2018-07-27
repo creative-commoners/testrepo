3811246e1e986b0f91e5da2b62cc7151efe4670c
Robbie Averill <robbie@silverstripe.com>
2018-07-27 13:56:22 +1200

    Add 2.1.x-dev project without bulkeditingtools requirements

---
+++ app/src/Page.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace {
+
+    use CWP\CWP\PageTypes\BasePage;
+    use SilverStripe\CMS\Model\SiteTree;
+
+    class Page extends BasePage
+    {
+        private static $db = [];
+
+        private static $has_one = [];
+    }
+}
