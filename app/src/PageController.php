1bb0619432d3f804f15e6c55837574409672428e
Garion Herman <garion@silverstripe.com>
2019-08-20 14:16:06 +1200

    CWP 2.4.x (replicating 2.4.0-rc1)

---
+++ app/src/PageController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace {
+
+    use SilverStripe\CMS\Controllers\ContentController;
+    use SilverStripe\Control\Middleware\HTTPCacheControlMiddleware;
+
+    class PageController extends ContentController
+    {
+        /**
+         * An array of actions that can be accessed via a request. Each array element should be an action name, and the
+         * permissions or conditions required to allow the user to access it.
+         *
+         * <code>
+         * [
+         *     'action', // anyone can access this action
+         *     'action' => true, // same as above
+         *     'action' => 'ADMIN', // you must have ADMIN permissions to access this action
+         *     'action' => '->checkAction' // you can only access this action if $this->checkAction() returns true
+         * ];
+         * </code>
+         *
+         * @var array
+         */
+        private static $allowed_actions = [];
+
+        protected function init()
+        {
+            HTTPCacheControlMiddleware::singleton()
+                ->enableCache(false, 60);
+
+            parent::init();
+        }
+    }
+}
