08a1b8b600debc282e0b23814d47163f2f2e3325
Garion Herman <garion@silverstripe.com>
2019-12-18 11:49:38 +1300

    Kitchen Sink 2.5.0-rc2

---
+++ app/src/Page.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace {
+
+    use CWP\CWP\PageTypes\BasePage;
+
+    class Page extends BasePage
+    {
+        private static $db = [];
+
+        private static $has_one = [];
+    }
+}
