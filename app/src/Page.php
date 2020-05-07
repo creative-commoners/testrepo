e0b0bd1333c664f603bf4af570a7799e86205075
Steve Boyd <emteknetnz@gmail.com>
2020-05-07 17:08:13 +1200

    React

--- app/src/Page.php
+++ app/src/Page.php
@@ -2,12 +2,23 @@
 
 namespace {
 
+    use SilverStripe\AssetAdmin\Forms\UploadField;
     use SilverStripe\CMS\Model\SiteTree;
+    use SilverStripe\Assets\File;
 
     class Page extends SiteTree
     {
         private static $db = [];
 
-        private static $has_one = [];
+        private static $has_one = [
+            'MyUploadField' => File::class
+        ];
+
+        public function getCMSFields()
+        {
+            $fields = parent::getCMSFields();
+            $fields->addFieldToTab('Root.Main', new UploadField('MyUploadField'));
+            return $fields;
+        }
     }
 }
